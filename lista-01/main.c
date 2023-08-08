929aa0936f7bdc7ba489a6a9b175dbfc59c941b8
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-08-08 09:20:20 -0300

    Criando o main.c

--- lista-01/main.c
+++ lista-01/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Bem vindo!");
+    printf("1- Encontrar o maior valor");
+    printf("2- Sorteie um número de forma pseudo aleatória");
+    printf("3- Informar números pares e impares");
+    printf("4- Simuador de jogo de axar");
+    printf("5- Elemento excluido do vetor");
+    printf("6- Matriz de identidade");
+    printf("7- Palindromo");
+    printf("8- Contador de vogal");
+
     return 0;
 }
