219801068be74d25541950fce965a0c090625b9f
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-08-22 08:25:02 -0300

    exemplo aloca memória

--- aula_8/main.c
+++ aula_8/main.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void aloca_memoria_pilha(int quantidade){
+    int tamanho = quantidade * (1 << 20);
+    char memoria[tamanho];
+    printf("Vetor do teclado: %d kB\n", tamanho/1024);
+    printf("Endereço da variável tamanho: %p\n", (void)&tamanho);
+    printf("Distancia entre vetor e tamanho: %ld\n", (((char*)&tamanho-memoria)/1024));
+    bzero(memoria, tamanho);
+    printf("zerou memória\n");
+}
+
+
 
 int mult_ite(int m, int n){
     int resultado = 0;
@@ -16,7 +30,12 @@ int mult_rec(int m, int n){
 }
 
 
-int main() {
+int main(int argc, char *argv[]) {
+    if(argc> 0){
+        aloca_memoria_pilha(strtol(argv[1],NULL, 10);
+    }
+
+
     printf("%d\n", mult_ite(2,6));
     printf("%d\n", mult_rec(2,6));
     return 0;
