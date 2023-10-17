8451110972074945f0860cd84c70ce281fd5e6ce
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-10-17 09:17:44 -0300

    bubble sort na main.c

--- aula_vet_rand/main.c
+++ aula_vet_rand/main.c
@@ -1,33 +1,52 @@
 #include <stdio.h>
 #include <locale.h>
-int main() {
-    setlocale(LC_ALL, "Portuguese");
-    int vetor[10]={32,12,45,23,55,98,53,11,3,65};
-    int pos_menor, menor, i,j;
 
-
-    printf("O conteúdo no vetor é: ");
+void bubble_sort(int vetor[10]){
+    int aux;
     for (int i = 0; i < 10; ++i) {
-        printf("%d ", vetor[i]);
-    }
-    printf("\n");
-    printf("O conteúdo no vetor agora é: ");
-
-    for (i = 0; i < 10; ++i) {
-        menor = vetor[i];
-        pos_menor = i;
-        for (j = i + 1; j < 10; ++j) {
-            if(vetor[j] < menor){
-                menor = vetor[j];
-                pos_menor = j;
+        for (int j = 0; j < 10-1; ++j) {
+            if(vetor[j]< vetor[j+1]){
+                aux = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = aux;
             }
         }
-        vetor[pos_menor] = vetor[i];
-        vetor[i] = menor;
+
     }
-    for (i = 0; i < 10; ++i) {
-        printf("%d ", vetor[i]);
-    }
+}
+
+
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+int vetor[10]={32,12,45,23,55,98,53,11,3,65};
+    bubble_sort(vetor);
+//    int pos_menor, menor, i,j;
+//
+//
+//    printf("O conteúdo no vetor é: ");
+//    for (int i = 0; i < 10; ++i) {
+//        printf("%d ", vetor[i]);
+//    }
+//    printf("\n");
+//    printf("O conteúdo no vetor agora é: ");
+//
+//    for (i = 0; i < 10; ++i) {
+//        menor = vetor[i];
+//        pos_menor = i;
+//        for (j = i + 1; j < 10; ++j) {
+//            if(vetor[j] < menor){
+//                menor = vetor[j];
+//                pos_menor = j;
+//            }
+//        }
+//        vetor[pos_menor] = vetor[i];
+//        vetor[i] = menor;
+//    }
+//    for (i = 0; i < 10; ++i) {
+//        printf("%d ", vetor[i]);
+//    }
 
 
 
