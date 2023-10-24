234c0a01b161a16d602977381929e0f0f93e5367
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-10-24 07:50:24 -0300

    .

--- aula_vet_rand/main.c
+++ aula_vet_rand/main.c
@@ -1,17 +1,93 @@
 #include <stdio.h>
 #include <locale.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+int criar_vetor(int vetor[], int tamanho){
+    vetor = (int*) malloc(tamanho * sizeof(int));
+    srand((unsigned) time(NULL));
+    for (int i = 0; i < tamanho; ++i) {
+        vetor[i] = rand() % 10 +  1;
+    }
+    return  vetor;
+}
+
+
+
+void imprimir(int vetor[10]){
+    for (int i = 0; i < 10; ++i) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+void nosso(int vetor[], int tamanho){
+
+    int pos_menor, menor, i,j;
+    printf("O conteúdo no vetor é: ");
+    for (int i = 0; i < tamanho; ++i) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+    printf("O conteúdo no vetor agora é: ");
+
+    for (i = 0; i < tamanho; ++i) {
+        menor = vetor[i];
+        pos_menor = i;
+        for (j = i + 1; j < tamanho; ++j) {
+            if(vetor[j] < menor){
+                menor = vetor[j];
+                pos_menor = j;
+            }
+        }
+        vetor[pos_menor] = vetor[i];
+        vetor[i] = menor;
+    }
+}
+
+void selection_sort(int vetor[], int tamanho){
+    int min;
+    int aux;
+    for (int i = 0; i < tamanho; i++) {
+        min = i;
+        for (int j = i + 1; j < tamanho ; j++) {
+            if(vetor[j] < vetor[min]){
+                min = j;
+            }
+        }
+        if(i != min){
+            aux= vetor[i];
+            vetor[i] = vetor[min];
+            vetor[min] = aux;
+        }
+    }
+}
+
+
+void insert_sort(int vetor[], int tamanho){
+    int chave;
+    int j;
+    for (int i = 0; i < tamanho; ++i) {
+        chave = vetor[i];
+        j = i-1;
+        while (j >= 0 && vetor[j] > chave){
+            vetor[j+1] = vetor[j];
+            j = j -1;
+        }
+        vetor[j+1] = chave;
+    }
+}
 
 void bubble_sort(int vetor[10]){
     int aux;
     for (int i = 0; i < 10; ++i) {
         for (int j = 0; j < 10-1; ++j) {
-            if(vetor[j]< vetor[j+1]){
+            if(vetor[j] > vetor[j+1]){
                 aux = vetor[j];
                 vetor[j] = vetor[j+1];
                 vetor[j+1] = aux;
             }
         }
-
     }
 }
 
@@ -20,35 +96,15 @@ void bubble_sort(int vetor[10]){
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-int vetor[10]={32,12,45,23,55,98,53,11,3,65};
-    bubble_sort(vetor);
-//    int pos_menor, menor, i,j;
-//
-//
-//    printf("O conteúdo no vetor é: ");
-//    for (int i = 0; i < 10; ++i) {
-//        printf("%d ", vetor[i]);
-//    }
-//    printf("\n");
-//    printf("O conteúdo no vetor agora é: ");
-//
-//    for (i = 0; i < 10; ++i) {
-//        menor = vetor[i];
-//        pos_menor = i;
-//        for (j = i + 1; j < 10; ++j) {
-//            if(vetor[j] < menor){
-//                menor = vetor[j];
-//                pos_menor = j;
-//            }
-//        }
-//        vetor[pos_menor] = vetor[i];
-//        vetor[i] = menor;
-//    }
-//    for (i = 0; i < 10; ++i) {
-//        printf("%d ", vetor[i]);
-//    }
+    int *v;
+    int n = 10;
+    criar_vetor(v,n);
 
-
+    //bubble_sort(vetor);
+    //insert_sort(v, 10);
+   // imprimir(v);
+    selection_sort(v, 10);
+    imprimir(v);
 
     return 0;
 }
