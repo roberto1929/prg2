512ee2d65f862ef050579ef3821ef981390e2e40
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-11-13 11:23:37 -0300

    Exercicios de arquivos

--- app_textos/main.c
+++ app_textos/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main(void) {
+    FILE *file = fopen("texto.txt", "w");
+
+    if(file == NULL){
+        printf("Erro ao abrir o arquivo");
+        return 1;
+    }
+
+    for (int i = 0; i < INTMAX_MAX; i++) {
+        fprintf(file, "%d\n", i);
+    }
+
+    fclose(file);
+
+    printf("Arquivo gerado com sucesso, texto.txt");
+
+
     return 0;
 }
