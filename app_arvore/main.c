7656e66e8f5c16ae7b214acc05ea97d0f22539b8
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-11-27 14:55:41 -0300

    Update arvore binaria

--- app_arvore/main.c
+++ app_arvore/main.c
@@ -1,33 +1,31 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
-
-clock_t start = 0, end;
-struct timeval inicio, fim;
-
+#include <stdlib.h>
 
 int main() {
-    int escolha = 0;
-    arvore_t arvore;
-    int n;
+    clock_t start = 0, end;
+    struct timeval inicio, fim;
 
-    while (1) {
-        printf("Bem vindo, escolha entre 1 e 2");
-        scanf("%d", escolha);
-        switch (escolha) {
-            case 1:
-                printf("Entre com um n");
-                scanf("%d", &n);
-                criar_arvore(n);
-                break;
-            case 2:
-                printf("Inserir números na árvore de busca binária");
-                comeca(&inicio);
-                inserir_valor(&arvore, n);
-                double tempo_de_cpu = medir_tempo_cpu(start);
-                printf("Tempo de CPU: ", tempo_de_cpu);
-                break;
-        }
-    }
+        int escolha = 0;
+            arvore_t arvore;
+            int n;
+            double tempo_de_cpu, tempo_parede;
+
+            printf("Entre com um n: ");
+            scanf("%d", &n);
+            criar_arvore(n);
+
+            printf("Inserir números na árvore de busca binária\n");
+            comeca(&inicio);
+            inserir_valor(&arvore, n);
+            tempo_de_cpu = medir_tempo_cpu(start);
+            printf("Tempo de CPU: %f\n", tempo_de_cpu);
+            free(arvore);
+            comeca(&inicio);
+            inserir_valor(&arvore, n);
+            tempo_parede = medir_tempo_cpu(start);
+            printf("Tempo de parede: %f", tempo_parede);
+            free(arvore);
 
     return 0;
 }
