8e7cc868c9d6db7d55c9edb7b4a14f4668b93af0
Roberto da SIlva Espindola <roberto.se@aluno.ifsc.edu.br>
2023-12-04 11:30:53 -0300

    Update de busca binaria em arvore

--- app_arvore_binaria/main.c
+++ app_arvore_binaria/main.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
 
+int num_aleatorio(int min, int max){
+    return min + rand() % (max - min + 1);
+}
 
 int main() {
-    printf("Hello, World!\n");
+    arvore_t arvore;
+    arvore_t *raiz = NULL;
+    int n = 5;
+    int num, valor_usuario, numero_removido;
+
+    //cria minha arvore
+    criar_arvore(n);
+
+    // tomada de tempo pra inserir valores na árvore
+    for (int i = 0; i < n; ++i) {
+        num = num_aleatorio(50,100);
+        raiz = inserir_valor(raiz,num);
+    }
+
+    printf("Arvore in ordem: ");
+    imprime_arvore_in_order(raiz);
+    printf("\n");
+
+    printf("Arvore pre ordem: ");
+    imprime_arvore_pre_order(raiz);
+    printf("\n");
+
+//    printf("Arvore pos ordem: ");
+//    imprime_arvore_post_order(raiz);
+//    printf("\n");
+
+    printf("strict graph{\n");
+    imprime_arvore_grafo(raiz);
+    printf("}\n");
+
+
+
+    printf("Entre com um valor inteiro para verificar se está na árvore: ");
+    scanf("%d", &valor_usuario);
+    imprime_filho_nivel(raiz,valor_usuario,1);
+
+    printf("Entre com um valor inteiro para remove-lo da árvore: ");
+    scanf("%d", &numero_removido);
+    raiz = remover_numero(raiz,numero_removido);
+
+    printf("Árvore após a remoção: ");
+    imprime_arvore_in_order(raiz);
+    printf("\n");
+
+
+    destruir_arvore(raiz);
+
     return 0;
 }
