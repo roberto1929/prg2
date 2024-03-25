97c55d37cf8240d8052d8b56b3e3398a4e358745
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2024-03-25 11:27:54 -0300

    Criado exercicio de fila

--- app_fila/main.c
+++ app_fila/main.c
@@ -1,59 +1,59 @@
 #include <stdio.h>
 #include <libprg/libprg.h>
 
-int main() {
-    fila_t minha_fila;
-    criar_fila(&minha_fila);
+typedef struct {
+    int op, elemento, temp, deq;
+} temp_t;
 
-    int escolha, elemento;
 
-    while (1) {
-        printf("Menu:\n");
-        printf("1. Criar uma fila\n");
-        printf("2. Inserir um novo elemento\n");
-        printf("3. Remover um elemento\n");
-        printf("4. Sair\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &escolha);
+int main(void){
 
-        switch (escolha) {
+    temp_t temp;
+    fila_t fila;
+    printf("Bem vindo ao programa de teste de fila\n");
+    while (1){
+        printf("MENU\n");
+        printf("1- Criar uma fila\n");
+        printf("2- Inserir um novo elemento;\n");
+        printf("3- Remover um elemento\n");
+        printf("Selecione a opção:");
+        scanf("%d", &temp.op);
+
+        switch (temp.op) {
             case 1:
-                printf("Digite o tamanho da fila: ");
-                int tamanho;
-                scanf("%d", &tamanho);
-                criar_fila(&minha_fila, tamanho);
-                printf("Fila criada com sucesso!\n");
+                printf("Etre com o tamanho da fila:");
+                scanf("%d", &fila.fila->tamanho);
+                if(cria(fila.fila, fila.fila->tamanho) == -1){
+                    printf("Estouro de memoria");
+                };
                 break;
-
             case 2:
-                if (full(&minha_fila)) {
-                    printf("A fila está cheia. Não é possível inserir mais elementos.\n");
+                printf("Entre com o elemento que será inserido:");
+                scanf("%d", temp.elemento);
+                if(enqueue(&fila, temp.elemento) == -1){
+                    printf("Não há espaço na fila");
                 } else {
-                    printf("Digite o elemento a ser inserido: ");
-                    scanf("%d", &elemento);
-                    enqueue(elemento, &minha_fila);
-                    printf("Elemento inserido com sucesso!\n");
-                }
-                break;
-
+                    for (int i = 0; i < fila.fila->total; ++i) {
+                        printf("%d", fila.fila->vetor[i]);
+                    }
+                    printf("\n");
+                }break;
             case 3:
-                if (empty(&minha_fila)) {
-                    printf("A fila está vazia. Não é possível remover um elemento.\n");
+                temp.temp = dequeue(&fila);
+                if(temp.temp == -1){
+                    printf("A fila está vazia");
                 } else {
-                    int elemento_removido = dequeue(&minha_fila);
-                    printf("Elemento removido: %d\n", elemento_removido);
+                    printf("Elemento removido: %d\n", temp.temp);
+                    temp.deq--;
                 }
+                printf("\n");
                 break;
-
-            case 4:
-                printf("Saindo...\n");
+            case 0:
+                printf("Encerrando programa\n");
+                libera_memoria(&fila);
                 return 0;
-
             default:
-                printf("Opção inválida. Tente novamente.\n");
-                break;
+                printf("Opção invalida\n");
         }
     }
-
-    return 0;
 }
