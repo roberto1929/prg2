a2495a30ceb8017ab4f1ac61b3cf17655637bc9d
Roberto da Silva Espindola <roberto.se@gmail.com>
2024-04-08 10:37:09 -0300

    Criado exercicio 2 de pilha

--- app_pilha/main.c
+++ app_pilha/main.c
@@ -4,49 +4,49 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Uso: RPNCalculator <equacao em RPN>\n");
-        return 1;
+
+    if(argc != 2){
+        printf("Uso: %s <expressão RPN>", argv[0]);
     }
+    pilha_t* pilha;
+    criar_pilha(pilha, strlen(argv[1]));
+    char* token = strtok(argv[1], " ");
 
-    Pilha pilha;
-    inicializar(&pilha);
-
-    char *equacao = argv[1];
-
-    for (int i = 0; equacao[i] != '\0'; i++) {
-        char token = equacao[i];
-        if (token >= '0' && token <= '9') {
-            push(&pilha, token - '0'); // Converte o caractere numérico para int
-        } else if (token == '+') {
-            int operand2 = pop(&pilha);
-            int operand1 = pop(&pilha);
-            push(&pilha, operand1 + operand2);
-        } else if (token == '-') {
-            int operand2 = pop(&pilha);
-            int operand1 = pop(&pilha);
-            push(&pilha, operand1 - operand2);
-        } else if (token == '*') {
-            int operand2 = pop(&pilha);
-            int operand1 = pop(&pilha);
-            push(&pilha, operand1 * operand2);
-        } else if (token == '/') {
-            int operand2 = pop(&pilha);
-            int operand1 = pop(&pilha);
-            if (operand2 == 0) {
-                printf("Erro: divisão por zero\n");
-                return 1;
+    while (token != NULL){
+        if(isdigit(*token)){
+            push(pilha, atoi(token));
+        } else {
+            int op1 = pop(pilha);
+            int op2 = pop(pilha);
+            switch (*token) {
+                case '+':
+                    push(pilha, op1 + op2);
+                    break;
+                case '-':
+                    push(pilha, op1 - op2);
+                    break;
+                case '*':
+                    push(pilha, op1 * op2);
+                    break;
+                case '/':
+                    push(pilha, op1 / op2);
+                    break;
+                default:
+                    printf("Operador inválido: %c\n", *token);
+                    return -1;
             }
-            push(&pilha, operand1 / operand2);
         }
+        token = strtok(NULL, " ");
     }
 
-    if (size(&pilha) == 1) {
-        int resultado = pop(&pilha);
+    int resultado = pop(pilha);
+    free(pilha->vetor);
+
+
+    if(empty_p(pilha)){
         printf("Resultado: %d\n", resultado);
     } else {
-        printf("Erro: formato de equação em RPN inválido\n");
-        return 1;
+        printf("Erro ao avaliar a expressão\n");
     }
 
     return 0;
