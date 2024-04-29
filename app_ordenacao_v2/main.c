6b65dcf5aeaab0a51915f65c725c42ab3b041f1b
Roberto da Silva Espindola <roberto.se@aluno.ifsc.edu.br>
2024-04-29 07:25:02 -0300

    Começando ordenação

--- app_ordenacao_v2/main.c
+++ app_ordenacao_v2/main.c
@@ -1,4 +1,3 @@
-
 #include <libprg/libprg.h>
 
 void imprimir(int *vetor){
@@ -9,98 +8,6 @@ void imprimir(int *vetor){
 }
 
 int main() {
-    int n = 0;
-    int right = 0, left = 0, s,e;
-    printf("Entre com o tamanho do vetor: ");
-    scanf("%d", &n);
-    sort_t sort;
-    sort.n = n;
-    printf("Tamanho do vetor: %d\n", n);
-    printf("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−\n");
-    printf("Algoritmo Relógio (seg) CPU (seg)\n");
-    printf("------------------------------------\n");
-
-
-    sort.vetor = cria_arranjo(n);
-
-    clock_t start = 0, end;
-    struct timeval inicio, fim;
-
-
-    //insertion sort
-        //CPU
-    comeca(&inicio);
-    insert_sort(&sort);
-    double tempo_cpu = medir_tempo_cpu(start);
-    printf("Inserção   %f      ", tempo_cpu);
-
-        //parede
-    comeca(&inicio);
-    insert_sort(&sort);
-    double tempo_parede = medir_tempo_parede(&inicio);
-    printf("%f\n", tempo_parede);
-
-
-    // selection sort
-    cria_arranjo(n);
-        //CPU
-    comeca(&inicio);
-    selection_sort(&sort);
-    tempo_cpu = medir_tempo_cpu(start);
-    printf("Seleção    %f      ", tempo_cpu);
-
-        //parede
-    comeca(&inicio);
-    selection_sort(&sort);
-    tempo_parede = medir_tempo_parede(&inicio);
-    printf("%f\n", tempo_parede);
-    free(sort.vetor);
-
-
-    // bubble sort
-    cria_arranjo(n);
-        //CPU
-    comeca(&inicio);
-    bubble_sort(&sort);
-    tempo_cpu = medir_tempo_cpu(start);
-    printf("Bolha      %f      ",tempo_cpu);
-        //parede
-    comeca(&inicio);
-    bubble_sort(&sort);
-    tempo_parede = medir_tempo_parede(&inicio);
-    printf("%f\n", tempo_parede);
-    free(sort.vetor);
-
-// merge sort
-    cria_arranjo(n);
-//CPU
-    comeca(&inicio);
-    merge_sort(&sort, left, right);
-    tempo_cpu = medir_tempo_cpu(start);
-    printf("Merge      %f      ", tempo_cpu);
-//PAREDE
-    comeca(&inicio);
-    merge_sort(&sort, left, right);
-    tempo_parede = medir_tempo_parede(&inicio);
-    printf("%f\n", tempo_parede);
-    free(sort.vetor);
-
-
-    // quick sort
-    cria_arranjo(n);
-//CPU
-    comeca(&inicio);
-    quick_sort(&sort, s, e);
-    tempo_cpu = medir_tempo_cpu(start);
-    printf("Quick      %f      ", tempo_cpu);
-//PAREDE
-    comeca(&inicio);
-    quick_sort(&sort, s, e);
-    tempo_parede = medir_tempo_parede(&inicio);
-    printf("%f\n", tempo_parede);
-
-    printf("------------------------------------\n");
-    free(sort.vetor);
 
 
     return 0;
